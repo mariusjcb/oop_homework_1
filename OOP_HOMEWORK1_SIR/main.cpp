9370032d62967f36d55d8b6a78186e242fd95db7
Marius Ilie <mariusjcb@Mac-Pro.local>
2017-03-03 03:12:47 -0800

    Initial Commit

---
+++ OOP_HOMEWORK1_SIR/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOP_HOMEWORK1_SIR
+//
+//  Created by Marius Ilie on 03/03/2017.
+//  Copyright © 2017 Marius Ilie @ FMI 143. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
